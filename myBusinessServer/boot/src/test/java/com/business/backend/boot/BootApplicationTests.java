2fca3c4db89436b29ce6fc456cb6d0c6cdf91a9c
dtorrensf <dtorrensfarias@outlook.com>
2023-12-08 18:48:39 +0100

    feat: add boot dependencies

---
+++ myBusinessServer/boot/src/test/java/com/business/backend/boot/BootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.backend.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
