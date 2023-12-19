bf162d3534802f4d56f966bc6f079412d884c240
dtorrensf <dtorrensfarias@outlook.com>
2023-12-19 22:04:04 +0100

    fix: boot package name

--- myBusinessServer/boot/src/main/java/com/business/backend/BootApplication.java
+++ myBusinessServer/boot/src/main/java/com/business/backend/BootApplication.java
@@ -1,4 +1,4 @@
-package com.business.backend.boot;
+package com.business.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- myBusinessServer/boot/src/test/java/com/business/backend/BootApplicationTests.java
+++ myBusinessServer/boot/src/test/java/com/business/backend/BootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.business.backend.boot;
+package com.business.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
